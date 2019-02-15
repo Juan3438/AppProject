d10949dd0ff671b1938c68d877e83e5c07cee6ba
sheba sow <ssow8171@bths.edu>
2019-02-14 20:13:38 -0500

    still not done

--- src/Controller.java
+++ src/Controller.java
@@ -1,7 +1,12 @@
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 
 public class Controller
@@ -9,7 +14,8 @@ public class Controller
     @FXML
     private ImageView pikachu;
 
-    public void makePikachu(MouseEvent c, String imageName) {
+    public void makePikachu(MouseEvent c) {
+        System.out.println("pit");
         Picture pic = new Picture("Pikachu.jpg");
     }
     public void makeJellyfish(MouseEvent c) {
@@ -19,14 +25,22 @@ public class Controller
         Picture pic = new Picture("redMotorcycle.jpg");
     }
     public void makeFlower(MouseEvent c) {
-        Picture pic = new Picture("Pikachu.jpg");
+        Picture pic = new Picture("Chrysanthemum.jpg");
     }
+    public class Main extends Application {
+        @Override
+        public void start(Stage primaryStage) throws Exception {
+            Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+            primaryStage.setTitle("Hello World");
+            primaryStage.setScene(new Scene(root, 300, 275));
+            primaryStage.show();
+        }
 
-    public static void main(String[] args)
-    {
-        Picture pix = new Picture("Chrysanthemum.jpg");
+
+        public static void main(String[] args) {
+            launch(args);
+        }
     }
-
     public static void changeSrc(Picture pix, String filename)
     {
         pix.setFileName(filename);
